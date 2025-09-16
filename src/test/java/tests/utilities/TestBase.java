645eca831039f15282ff9aedc0561c6fd2d7e2cc
ZahraAleid <zahra.eid112@gmail.com>
2025-09-16 15:52:37 +0300

    Add screenshot functionality to TestBase class

--- src/test/java/tests/utilities/TestBase.java
+++ src/test/java/tests/utilities/TestBase.java
@@ -1,11 +1,20 @@
 package tests.utilities;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
   /*
         TestBase Class:
@@ -50,5 +59,36 @@ public class TestBase {
     void tearDown() {
         driver.quit();
     }
+
+    protected void takeScreenshot(WebDriver driver) {
+        TakesScreenshot ss = (TakesScreenshot) driver;
+        File screenFile = ss.getScreenshotAs(OutputType.FILE);
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd:HH-mm-ss"));
+        String screenShotName = "Screenshot" + timeStamp + ".png";
+        Path screenShotPath = Path.of(System.getProperty("user.dir"), "target", "test-output", "screenshots", screenShotName);
+
+        try {
+            FileUtils.copyFile(screenFile, screenShotPath.toFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    protected void takeScreenshotOfElement(WebDriver driver, WebElement element) {
+        File screenShotFile = element.getScreenshotAs(OutputType.FILE);
+        String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd:HH-mm-ss"));
+        String screenShotName = "ElementScreenshot" + timeStamp + ".png";
+        Path screenShotPath = Path.of(System.getProperty("user.dir"), "target", "test-output", "screenshots", screenShotName);
+
+        try {
+            FileUtils.copyFile(screenShotFile, screenShotPath.toFile());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 
 }
